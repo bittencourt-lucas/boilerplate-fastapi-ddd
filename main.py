5028f383dba798c824cbc89cb6ae487913672c6b
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-04-05 19:28:13 -0300

    chore: created basic empty router and imported it in main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.api.routers import router
 
 app = FastAPI()
 
@@ -10,3 +11,5 @@ app.add_middleware(
   allow_methods=["*"],
   allow_headers=["*"],
 )
+
+app.include_router(router)
