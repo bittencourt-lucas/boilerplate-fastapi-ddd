3b12f02d2a6910895de8dcf0a8f936b807d43bea
Lucas Bittencourt <lucascafieiro@hotmail.com>
2023-04-05 19:38:00 -0300

    chore: updated styling to fit PEP 8 standard

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from app.api.routers import router
 app = FastAPI()
 
 app.add_middleware(
-  CORSMiddleware,
-  allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
